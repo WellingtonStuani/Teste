6cf79ed4395b91f193a721db738f4c2b0d3b5ca5
wesley <wesley@wesleystuani>
2017-04-20 18:01:00 -0300

    testando  tela

---
+++ Trabalho1bim3semestre/src/br/univel/Coluna.java
@@ -0,0 +1,25 @@
+package br.univel;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.FIELD)
+	public @interface Coluna {
+
+
+		String nome() default "";
+
+		int tamanho() default 255;
+		
+		boolean pk() default false;
+		
+	}
+
+	
+
